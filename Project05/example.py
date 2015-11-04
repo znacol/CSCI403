1b483e4bf0f2a058870f2be30c469d58de1d5a57
Zoe Nacol <znacol@mymail.mines.edu>
2015-11-04 15:50:52 -0700

    Notes added.

--- Project05/example.py
+++ Project05/example.py
@@ -1,53 +1,96 @@
 #!/usr/bin/python3
-
-# 
+# Zoe Nacol
+# CSCI 403 Fall 2015
+# Project 05
 #
-# CSCI 403 example Python script
-#
-# Author: C. Painter-Wakefield
+# Some code taken from C. Painter-Wakefield's provided example file
 # Modified: 10/31/2015
-#
-# This script does a couple of SELECT queries against tables in public, then
-# attempts some modification queries on the project5 data.  Examples are
-# given of prepared queries using parameters, exception handling, and 
-# very basic input/output.
 
 import getpass
 import pg8000
 
-login = input('login: ')
-secret = getpass.getpass('password: ')
+def main():
+    cursor = db.cursor()
+    connect() 
+    
+    ans=True
+    while ans:
+        print ("""
+        1. Search
+        2. Insert
+        3. Modify
+        4. Delete
+        5. Exit/Quit
+        """)
+        ans=raw_input("Please enter your selection") 
+        if ans=="1": 
+            search()
+            print("\n Search") 
+        elif ans=="2":
+            insert()
+            print("\n Insert") 
+        elif ans=="3":
+            modify()
+            print("\n Modify") 
+        elif ans=="4":
+            delete()
+            print("\n Delete") 
+        elif ans=="5":
+            print("\n Exiting!")
+            ans=None
+        elif ans !="":
+            print("\n Not Valid Choice Try again")
+    cursor.close()
+    db.close()
 
-credentials = {'user'    : login, 
-               'password': secret, 
-               'database': 'csci403',
-               'host'    : 'flowers.mines.edu'}
+# Connects to database
+def connect():
+    login = input('login: ')
+    secret = getpass.getpass('password: ')
 
-try:
-    db = pg8000.connect(**credentials)
-except pg8000.Error as e:
-    print('Database error: ', e.args[2])
-    exit()
+    credentials = {'user'    : login, 
+                   'password': secret, 
+                   'database': 'csci403',
+                   'host'    : 'flowers.mines.edu'}
 
-# uncomment next line if you want every insert/update/delete to immediately
-# be applied; you can remove all db.commit() and db.rollback() statements
-#db.autocommit = True
+    try:
+        db = pg8000.connect(**credentials)
+    except pg8000.Error as e:
+        print('Database error: ', e.args[2])
+        exit()
 
-cursor = db.cursor()
+    # uncomment next line if you want every insert/update/delete to immediately
+    # be applied; you can remove all db.commit() and db.rollback() statements
+    db.autocommit = True
+    return
+def search();
+        ans=True
+        print ("""
+        1. Artists
+        2. Genre
+        3. Keyword
+        """)
+        ans=raw_input("Please enter your selection: ") 
+        if ans=="1": 
+            print("\n Please enter the artist name: ")
+            print("\n Search by artist") 
+        elif ans=="2":
+            print("\n Please enter the genre: ")
+            print("\n Search by genre") 
+        elif ans=="3":
+            print("\n Please enter the keyword: ")
+            print("\n Search by keyword") 
+        elif ans !="":
+            print("\n Not Valid Choice Try again")
+def insert():
 
-# immediate SELECT
-cursor.execute(
-   """SELECT course_id, section, title 
-      FROM mines_courses 
-      WHERE instructor = 'Painter-Wakefield, Christopher'"""
-)
+def modify():
 
-results = cursor.fetchall()
-for row in results:
-    course_id, section, title = row
-    print(course_id, section, title)
-print()
+def delete():
 
+
+
+########################################## CPW CODE ################################################
 # prepared SELECT
 faculty = input('Enter faculty as lastname, firstname: ')
 query = """SELECT course_id, section, title 
@@ -68,7 +111,6 @@ try:
     db.commit()
 except pg8000.Error as e:
     print('Database error: ', e.args[2])
-    db.rollback() # necessary after error, unless autocommitting
 
 # second time should cause an integrity constraint violation
 try:
@@ -76,7 +118,6 @@ try:
     db.commit()
 except pg8000.Error as e:
     print('Database error: ', e.args[2])
-    db.rollback() # necessary after error, unless autocommitting
 
 # bad SELECT query with exception handling
 query = "SELECT FROM foo WHERE blah = arg"
@@ -84,7 +125,3 @@ try:
     cursor.execute(query)
 except pg8000.Error as e:
     print('Database error: ', e.args[2])
- 
-cursor.close()
-db.close()
- 
